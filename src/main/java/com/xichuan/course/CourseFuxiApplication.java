7fa0308ae1be458c455737600a7146aae8e7df25
xichuanwushao <avatarwx@163.com>
2022-02-27 15:25:45 +0800

    创建springcloud项目 1.https://start.springboot.io/

---
+++ src/main/java/com/xichuan/course/CourseFuxiApplication.java
@@ -0,0 +1,13 @@
+package com.xichuan.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseFuxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseFuxiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xichuan/course/CourseFuxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xichuan.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseFuxiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
