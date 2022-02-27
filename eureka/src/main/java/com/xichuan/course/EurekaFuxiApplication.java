585c7e7ee6eb7db0a5042b515db0e2825d1065fa
xichuanwushao <avatarwx@163.com>
2022-02-27 16:54:15 +0800

    4.2-2 解决项目报错问题并正常运行

--- eureka/src/main/java/com/xichuan/course/EurekaFuxiApplication.java
+++ eureka/src/main/java/com/xichuan/course/EurekaFuxiApplication.java
@@ -1,13 +1,24 @@
 package com.xichuan.course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaFuxiApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EurekaFuxiApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(EurekaFuxiApplication.class, args);
+        SpringApplication app = new SpringApplication(EurekaFuxiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("Eureka地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
