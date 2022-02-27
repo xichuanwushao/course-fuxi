fbabd7c366abe9f50ceb44d47e52ad07395646d2
xichuanwushao <avatarwx@163.com>
2022-02-27 16:14:54 +0800

    4.2-1 将单maven项目改造为maven父子项目 1.将父模块依赖放入子模块 删除子模块依赖 2.删除父模块src目录 3.子模块新建运行类并运行 （增加eureka模块作为注册服务端 将单maven项目改造为maven父子项目） 4.使用Eureka搭建注册中心

--- eureka/src/main/java/com/xichuan/course/EurekaFuxiApplication.java
+++ eureka/src/main/java/com/xichuan/course/EurekaFuxiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseFuxiApplication {
+public class EurekaFuxiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CourseFuxiApplication.class, args);
+        SpringApplication.run(EurekaFuxiApplication.class, args);
     }
 
 }
