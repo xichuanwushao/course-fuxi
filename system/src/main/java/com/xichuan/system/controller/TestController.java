578ca4923c983b05bbab6781e3716680212a9d73
xichuanwushao <avatarwx@163.com>
2022-03-05 15:51:48 +0800

    3.1 mybatis使用示例 访问成功

--- system/src/main/java/com/xichuan/system/controller/TestController.java
+++ system/src/main/java/com/xichuan/system/controller/TestController.java
@@ -1,15 +1,26 @@
 package com.xichuan.system.controller;
 
-
+import com.xichuan.system.domain.Test;
+import com.xichuan.system.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 //@Controller 如果接口返回页面用Controller
 @RestController//@Controller 如果接口返回Json 用RestController
-@RequestMapping("system")
 public class TestController {
+    @Autowired
+    private TestService testService;
+
     @RequestMapping("test")
     public String test(){
         return "success";
     }
-}
 
+    @RequestMapping("testlist")
+    public List<Test> testlist(){
+        return testService.list();
+    }
+}
---
+++ system/src/main/java/com/xichuan/system/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package com.xichuan.system.mapper;
+
+
+import com.xichuan.system.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+    public List<Test> list();
+}
